7f98c56eaf6c60ff158765839ce7e677d809b71d
Emerson Vergara <ee-web@hotmail.com>
2022-09-02 10:46:32 -0500

    Desarrollo: Creacion de componentes ng g c  tuta/nombreDelComponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgComponent } from './components/img/img.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgComponent,
+    NavComponent,
+    ProductComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,

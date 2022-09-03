10df585da254bac1af1545263365b3e9550842a9
Emerson Vergara <ee-web@hotmail.com>
2022-09-02 22:47:55 -0500

    Reactividad con  rxjs

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,25 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ShoppingCartService } from '@app/services/shoppingcart.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit, OnDestroy {
   activeMenu = false;
   counter = 0;
+  
+  totalShoppingCart$!: Subscription;
 
-  constructor() {} //private storeService: StoreService
+  constructor(private shoppingCartService: ShoppingCartService) {} //private storeService: StoreService
 
   ngOnInit(): void {
-    /**
-    this.storeService.myCart$.subscribe(products => {
-      this.counter = products.length;
-    });
-    */
+    this.totalShoppingCart$ =
+      this.shoppingCartService.totalProductShoppingCart$.subscribe((total) => {
+        this.counter = total;
+      });
   }
 
   toggleMenu() {
     this.activeMenu = !this.activeMenu;
+  }
+
+  ngOnDestroy(): void {
+    this.totalShoppingCart$.unsubscribe();
   }
 }
--- src/app/services/shoppingcart.service.ts
+++ src/app/services/shoppingcart.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+
+import { BehaviorSubject } from 'rxjs';
 import { Product } from '@app/models/product.model';
 
 @Injectable({
@@ -6,17 +8,45 @@ import { Product } from '@app/models/product.model';
 })
 export class ShoppingCartService {
   private myShoppingCart: Product[] = [];
+  private totalPriceShoppingCart: BehaviorSubject<number> =
+    new BehaviorSubject<number>(0);
+  private totalProductShoppingCart: BehaviorSubject<number> =
+    new BehaviorSubject<number>(0);
+
+  totalPriceShoppingCart$ = this.totalPriceShoppingCart.asObservable();
+  totalProductShoppingCart$ = this.totalProductShoppingCart.asObservable();
+
   constructor() {}
 
   addProduct(product: Product) {
     this.myShoppingCart.push(product);
+    this.setTotalPrice();
+    this.setTotalProduct();
   }
 
   getTotalPrice(): number {
-    return this.myShoppingCart.reduce((sum, item) => sum + item.price, 0);
+    return this.calculateTotalPrice();
   }
 
   getTotalItem(): number {
+    return this.calculateTotalProduct();
+  }
+
+  private setTotalPrice(): void {
+    this.totalPriceShoppingCart.next(this.calculateTotalPrice());
+    //console.log('setTotalPrice ', this.totalPriceShoppingCart$);
+  }
+
+  private setTotalProduct(): void {
+    this.totalProductShoppingCart.next(this.calculateTotalProduct());
+    //console.log('setTotalProduct ', this.totalProductShoppingCart$);
+  }
+
+  private calculateTotalPrice(): number {
+    return this.myShoppingCart.reduce((sum, item) => sum + item.price, 0);
+  }
+
+  private calculateTotalProduct(): number {
     return this.myShoppingCart.length;
   }
 }
